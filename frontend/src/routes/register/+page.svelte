cb5b6568b6e00413e3261b83bbd6c38a82a5f7f6
Jannis Cloodt <j.cloodt@micromata.de>
2023-12-14 10:11:30 +0100

    register page passwordConfirm and accessibility

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+    let password = "";
+    let passwordConfirm = "";
+
+    $: passwordValid = password === passwordConfirm
+</script>
+
+
 <div class="flex items-center pb-80 justify-center h-screen">
     <form method="post" class="w-full max-w-md bg-white shadow-md rounded-md p-6">
         <div class="mb-4">
@@ -10,31 +18,38 @@
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-semibold mb-2">
                 E-Mail
-                <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="password">
+                <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="email">
             </label>
         </div>
 
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-semibold mb-2">
                 Password
-                <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="password">
+                <input bind:value={password} class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="password">
             </label>
         </div>
 
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-semibold mb-2">
                 Repeat Password
-                <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="password">
+                <input bind:value={passwordConfirm} class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="password">
             </label>
+            {#if !passwordValid}
+                <span class="text-red-600">Passwords are not identical</span>
+            {/if}
         </div>
 
-        <button class="w-full bg-indigo-500 text-white py-2 rounded-md hover:bg-indigo-600 focus:outline-none" type="submit">
+        <button class="w-full bg-[#5e60e4] text-white py-2 rounded-md hover:bg-indigo-600 focus:outline-none disabled:bg-gray-400"
+                type="submit"
+                disabled={!passwordValid}>
             Sign Up
         </button>
+
         <div class="pt-3">
             <a>Already have an Accounr? Click</a>
             <a href="../login" class="text-blue-700 underline">here</a>
         </div>
+
 
     </form>
 </div>
