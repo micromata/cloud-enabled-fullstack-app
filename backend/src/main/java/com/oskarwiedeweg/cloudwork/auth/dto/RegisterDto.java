450552c5a6456bd291492942c65348e19fa617dd
OskarWiedeweg <dev.wiedeweg@gmail.com>
2023-12-18 08:57:34 +0100

    fix: regex fix for username and password in registration

--- backend/src/main/java/com/oskarwiedeweg/cloudwork/auth/dto/RegisterDto.java
+++ backend/src/main/java/com/oskarwiedeweg/cloudwork/auth/dto/RegisterDto.java
@@ -10,7 +10,7 @@ import lombok.Data;
 public class RegisterDto {
 
     @NotBlank(message = "Is empty.")
-    @Pattern(regexp = "([A-Za-z0-9-._])", message = "Other character than A-Z, a-z, 0-9, -, ., or _")
+    @Pattern(regexp = "([A-Za-z0-9-._])*", message = "Other character than A-Z, a-z, 0-9, -, ., or _")
     @Size(min = 5, message = "Less characters than 5.")
     private final String username;
 
@@ -20,9 +20,9 @@ public class RegisterDto {
 
     @NotBlank(message = "Is empty.")
     @Pattern.List(value = {
-            @Pattern(regexp = "(?=.*[A-Z])", message = "No capital letter."),
-            @Pattern(regexp = "(?=.*[a-z])", message = "No lowercase letter."),
-            @Pattern(regexp = "(?=.*\\d)", message = "No digit.")
+            @Pattern(regexp = ".*([A-Z]).*", message = "No capital letter."),
+            @Pattern(regexp = ".*([a-z]).*", message = "No lowercase letter."),
+            @Pattern(regexp = ".*(\\d).*", message = "No digit.")
     })
     @Size(min = 8, message = "Less thant 8 characters.")
     private final String password;
