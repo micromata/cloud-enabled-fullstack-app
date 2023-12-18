f3134645b7e7efc95561529f9bcb78f14b961ed9
OskarWiedeweg <dev.wiedeweg@gmail.com>
2023-12-18 10:19:50 +0100

    feat: add input component

---
+++ frontend/src/lib/components/Input.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let label: string;
+    export let name: string;
+    export let type: string;
+
+    export let errors: string[] | undefined = undefined;
+    export let errorType: 'requirements' | 'message' = "requirements";
+</script>
+
+<div class="mb-4">
+    <label class="block text-gray-700 text-sm font-semibold mb-2">
+        {label}
+        <input name={name} class:border-red-600={errors} class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type={type}>
+    </label>
+    {#if (errors)}
+        {#if (errorType === "requirements")}
+            <div class="text-red-600">
+                <p>The following requirements are not met:</p>
+                <ul class="list-disc px-10">
+                    {#each errors as error}
+                        <li>{error}</li>
+                    {/each}
+                </ul>
+            </div>
+        {:else if (errorType === "message")}
+            {#each errors as error}
+                <p class="text-red-600">{error}</p>
+            {/each}
+        {/if}
+    {/if}
+</div>
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,25 +1,14 @@
 <script>
+    import Input from "$lib/components/Input.svelte";
+
     export let form;
 </script>
 
 <div class="flex items-center pb-80 justify-center h-screen">
     <form method="post" class="w-full max-w-md bg-white shadow-md rounded-md p-6">
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-semibold mb-2">
-                Username
-                <input name="username" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="text">
-            </label>
-        </div>
+        <Input label="Username" name="username" type="text"/>
 
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-semibold mb-2">
-                Password
-                <input name="password" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="password">
-            </label>
-            {#if (form?.error)}
-            <span class="text-red-600">{form.error}</span>
-            {/if}
-        </div>
+        <Input label="Password" name="password" type="password" errors={form?.error ? [form.error] : undefined} errorType="message"/>
 
         <button class="w-full bg-indigo-500 text-white py-2 rounded-md hover:bg-indigo-600 focus:outline-none" type="submit">
             Sign In
