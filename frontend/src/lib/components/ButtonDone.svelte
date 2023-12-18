c73c88841059a8ff4211437efb51589da3a6c559
Jannis Cloodt <j.cloodt@micromata.de>
2023-12-18 10:37:51 +0100

    ButtonDone Component

---
+++ frontend/src/lib/components/ButtonDone.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let titel: string;
+</script>
+
+<button
+        class="mt-5 w-full bg-indigo-500 text-white py-2 rounded-md hover:bg-indigo-600 focus:outline-none"
+>
+    {titel}
+</button>
