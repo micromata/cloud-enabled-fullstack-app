3c63794ec63cba7abc1472761d2cf72afb9fe805
Jannis Cloodt <j.cloodt@micromata.de>
2023-12-18 11:10:50 +0100

    Error handling in register Form

--- frontend/src/routes/register/+page.svelte
+++ frontend/src/routes/register/+page.svelte
@@ -1,48 +1,26 @@
 <script lang="ts">
+    import Input from "$lib/components/Input.svelte";
+
     let password = "";
     let passwordConfirm = "";
 
     $: passwordValid = password === passwordConfirm
 
     export let form;
+
 </script>
 
 
 <div class="flex items-center pb-80 justify-center h-screen">
     <form method="post" class="w-full max-w-md bg-white shadow-md rounded-md p-6">
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-semibold mb-2">
-                Username
-                <input name="username" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="text" required>
-            </label>
-        </div>
 
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-semibold mb-2">
-                E-Mail
-                <input name="email" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="email" required>
-            </label>
-        </div>
+        <Input name="username" label="Username" type="text" errors={form?.errors?.username}/>
 
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-semibold mb-2">
-                Password
-                <input name="password" bind:value={password} class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="password" required>
-            </label>
-        </div>
+        <Input name="email" label="E-Mail" type="email"/>
 
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-semibold mb-2">
-                Repeat Password
-                <input name="passwordConfirm" bind:value={passwordConfirm} class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="password" required>
-            </label>
-            {#if !passwordValid}
-                <span class="text-red-600">Passwords are not identical</span>
-            {/if}
-            {#if (form?.error)}
-                <span class="text-red-600">{form.error}</span>
-            {/if}
-        </div>
+        <Input name="password" label="Password" type="password" bind:value={password} errors={form?.errors?.password}/>
+
+        <Input name="passwordConfirm" label="Repeat Password" type="password" bind:value={passwordConfirm} errors={!passwordValid ? ["Password are not identical"] : undefined} errorType="message"/>
 
         <button class="w-full bg-[#5e60e4] text-white py-2 rounded-md hover:bg-indigo-600 focus:outline-none disabled:bg-gray-400"
                 type="submit"
