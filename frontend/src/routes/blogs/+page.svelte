8a6a771b25070253ca96d170c82d31903c2308f8
Jannis Cloodt <j.cloodt@micromata.de>
2023-12-18 15:15:09 +0100

    updated Alert

--- frontend/src/routes/blogs/+page.svelte
+++ frontend/src/routes/blogs/+page.svelte
@@ -21,9 +21,7 @@
         <button class="bg-blue-500 text-white px-4 py-2 rounded disabled:bg-gray-400 ml-auto" onclick="window.location.href='/createFeed';" disabled={!$auth}>
             Create new Blog
         </button>
-        {/if}
-
-        {#if !$auth}
+        {:else}
         <span class="mt-3">
             <Alert headline="Please Log In to Post your Feed!" message="You can Log In " linkText="here." link="/login"/>
         </span>
@@ -52,6 +50,7 @@
             {/each}
         </div>
     </div>
+
 </main>
 
 
