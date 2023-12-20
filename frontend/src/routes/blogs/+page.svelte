0cb81d70df8b5d28e5516f7cfd16b78135629921
Jannis Cloodt <j.cloodt@micromata.de>
2023-12-20 09:51:45 +0100

    added page to show the selected Blog

--- frontend/src/routes/blogs/+page.svelte
+++ frontend/src/routes/blogs/+page.svelte
@@ -39,20 +39,24 @@
 
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-12 relative">
             {#each data.posts as post (post.id)}
-                <div class="flex flex-col rounded-md shadow-md overflow-hidden transition-transform transform hover:scale-105">
-                    {#if post.imageUrl}
-                        <img class="w-full h-48 object-cover" src={post.imageUrl} alt={post.title} />
+                <div class="flex flex-col rounded-md shadow-md overflow-hidden transition-transform transform hover:scale-105 h-full">
+                    {#if post.image}
+                        <img class="w-full h-48 object-cover" src={post.image} alt={post.title} />
                     {:else}
                         <div class="bg-gradient-to-br from-blue-500 to-blue-200 h-48"></div>
                     {/if}
                     <div class="p-6 flex-grow">
                         <h2 class="text-xl font-bold mb-2">{post.title}</h2>
-                        <p class="text-gray-600">{post.description}</p>
-                        <!-- Weitere Informationen oder Aktionen können hier hinzugefügt werden -->
+                        <p class="text-gray-600 overflow-hidden line-clamp-3">{post.preview}</p>
+                        <div class="mt-auto">
+                            <a href="/showBlog?id={post.id}" class="bg-blue-500 text-white px-4 py-2 rounded disabled:bg-gray-400">Weiterlesen</a>
+                            <!-- Weitere Informationen oder Aktionen können hier hinzugefügt werden -->
+                        </div>
                     </div>
                 </div>
             {/each}
         </div>
+
 
     </div>
 
