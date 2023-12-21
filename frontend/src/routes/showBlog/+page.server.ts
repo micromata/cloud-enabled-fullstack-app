a4fc42c56d4d5a7805e0e41d707a4196d474409b
Jannis Cloodt <j.cloodt@micromata.de>
2023-12-21 10:24:19 +0100

    added page to manage own blogs

--- frontend/src/routes/showBlog/+page.server.ts
+++ frontend/src/routes/showBlog/+page.server.ts
@@ -1,11 +1,16 @@
 import type {PageServerLoad} from "./$types";
 import {env} from "$env/dynamic/public";
 
-export const load: PageServerLoad = async ({fetch}) => {
+export const load: PageServerLoad = async ({fetch, url}) => {
 
-    const response = await fetch(env.PUBLIC_BACKEND_URL + "v1/feed", {
+    const id = url.searchParams.get("id")
+    const response = await fetch(env.PUBLIC_BACKEND_URL + `v1/feed/${id}`, {
         method: "GET"
     });
+    const responseData = await response.json()
+    console.log(responseData);
 
-    return await response.json();
+    return await responseData;
+
+
 }
