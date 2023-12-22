71e182dcd1059debafe2d6b5c1cc0496cfb614d6
Jannis Cloodt <j.cloodt@micromata.de>
2023-12-22 12:17:03 +0100

    error fix

--- backend/src/main/java/com/oskarwiedeweg/cloudwork/feed/dto/CommentPostDto.java
+++ backend/src/main/java/com/oskarwiedeweg/cloudwork/feed/dto/CommentPostDto.java
@@ -1,9 +1,11 @@
 package com.oskarwiedeweg.cloudwork.feed.dto;
 
 import com.oskarwiedeweg.cloudwork.user.UserDto;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 public class CommentPostDto {
     private Long id;
     private UserDto author;
